55aedc697c842cf22fb1fcef572e06644463e3c7
Abhishek Lal <abhisheklalnediya@gmail.com>
2019-04-21 19:39:34 +0530

    only admin can manage users

--- User/views.py
+++ User/views.py
@@ -1,12 +1,14 @@
 from User.models import User
 from rest_framework import generics
 from User.serializers import UserSerializer
-
+from rest_framework.permissions import IsAdminUser
 class UserList(generics.ListCreateAPIView):
+    permission_classes = [ IsAdminUser ]
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class UserDetail(generics.RetrieveUpdateAPIView):
+    permission_classes = [ IsAdminUser ]
     queryset = User.objects.all()
     serializer_class = UserSerializer
