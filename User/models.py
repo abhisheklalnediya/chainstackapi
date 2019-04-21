438985204d1b2dfdc42228afe88417bedc21ee6a
Abhishek Lal <abhisheklalnediya@gmail.com>
2019-04-21 18:46:04 +0530

    user names can be blank as we are using only email login

--- User/models.py
+++ User/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
+    username = models.CharField(max_length=30, unique=False)
     email = models.EmailField('email address', unique=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
