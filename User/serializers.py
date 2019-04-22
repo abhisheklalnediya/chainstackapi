dc905ebe406aec7004aa6a8c818fd258b52897be
Abhishek Lal <abhisheklalnediya@gmail.com>
2019-04-22 08:23:51 +0530

    set password for created user

--- User/serializers.py
+++ User/serializers.py
@@ -2,10 +2,15 @@ from User.models import User, QuotaCredit
 from rest_framework import serializers
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = ('id', 'email', 'quota')
+        fields = ('id', 'email', 'quota', "password")
         read_only_fields = ('quota',)
+    
+    def create(self, validated_data):
+        user = User.objects.create_user(username=validated_data["email"], **validated_data)
+        return user
     
 class QuotaCreditSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), required=True)
